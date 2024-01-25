c3c003a5d2041b12c91a4d6f6bc93034f52373fb
sittipak.sri <sittipak.ssw@gmail.com>
2024-01-25 22:19:08 +0700

    Add 5 page views

--- cn334/urls.py
+++ cn334/urls.py
@@ -20,8 +20,15 @@ from django.urls import path, include
 from ecommerce import views as ecom_views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ecommerce/', ecom_views.ecommerce_index_view),
-    path("ecommerce/item/<item_id>", ecom_views.item_view)
+    path("ecommerce/item/<item_id>", ecom_views.item_view),
+    path("ecommerce/homepage", ecom_views.homepage_view),
+    path("ecommerce/catagory", ecom_views.catagory_view),
+    path("ecommerce/product", ecom_views.product_view),
+    path("ecommerce/checkout", ecom_views.checkout_view),
+    path("ecommerce/contact", ecom_views.contact_view)
 ]
+
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -15,3 +15,23 @@ def item_view(request, item_id):
     }
 
     return render(request, 'index.html',context= context_data)
+
+def homepage_view(request):   
+    
+    return render(request, 'homepage.html')
+
+def catagory_view(request):   
+    
+    return render(request, 'catagory.html')
+
+def product_view(request):   
+    
+    return render(request, 'product.html')
+
+def checkout_view(request):   
+    
+    return render(request, 'checkout.html')
+
+def contact_view(request):   
+    
+    return render(request, 'contact.html')
